9a25ce55c6fd5b12db3eb413d1b8f9b59cfd41b4
Marc Tapp <marc.tapp@gmail.com>
2020-07-25 17:38:11 -0400

    added introtojs

---
+++ variables.js
@@ -0,0 +1,23 @@
+// Variables in javascript are used to store information you need to call or manipulate later
+
+// There are 3 ways to create a variable
+
+// var variable= "Var can be delared, and changed, and can also be redeclared, and they are not often used"
+// var variable= "If you did a console.log(variable) now, this string would print, not the string above"
+
+// let letVariable = "A let can be declared, and changed but never redeclared"
+// let letVariable = "If you tried to console.log(letVariable) you would get an error because the variable had already been declared"
+// letVariable = "Now if you tried to console.log(letVariable) you would see this string print out"
+
+// const constVariable = "A const means constant and can be declared, but never changed or redeclared"
+// const constVariable = "If you tried to console.log(constVariable) now, it throw an error saying constVariable had already been declared"
+// constVariable = "If you tried to console.log(constVariable) now, it would also throw an error saying the constVariable had already been declared"
+
+// Variables can hold any type of information
+
+let stringsVariable = "This variable contains a string as its value"
+let booleanVariable = true // this variable is a Boolean value, meaning its either True/False
+let numberVariable = 12344 // this variable contains a number as its value
+let arrayVariable = ["This", "variable's", "value", "is", "an", "array", "of", "strings"] 
+let objectVariable = {description: "This variable is an object", key: "Meaning that it consists of key/value pairs", thisisthekey: "this is the value"}
+let functionVariable = function(){console.log('this variable has a function for its value')}
